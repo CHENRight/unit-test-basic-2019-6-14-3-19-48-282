8ef1dcda9201825a04a665d9f8a4f78025983df2
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-15 15:12:37 +0800

    implement test to return expense type B when project 's type is external and name is 'Project B'

--- src/test/java/ExpenseService/ExpenseServiceTest.java
+++ src/test/java/ExpenseService/ExpenseServiceTest.java
@@ -33,9 +33,11 @@ class ExpenseServiceTest {
 
     @Test
     void should_return_expense_type_B_if_project_is_external_and_name_is_project_B() throws UnexpectedProjectTypeException {
-        // given
-        // when
-        // then
+        Project project = new Project(ProjectType.EXTERNAL,"Project B");
+
+        ExpenseType result = getExpenseCodeByProjectTypeAndName(project);
+
+        Assertions.assertEquals(ExpenseType.EXPENSE_TYPE_B,result);
     }
 
     @Test
